f55a19e94d4f387ab142c723300d14ee638a011c
Maurício Mafra Monnerat <monnerat.mauricio@gmail.com>
2023-05-01 17:56:36 -0300

    CRUD da Comanda e Produto

--- src/main/java/app/AppServer.java
+++ src/main/java/app/AppServer.java
@@ -2,6 +2,7 @@ package app;
 
 import app.entities.Comanda;
 import app.entities.ItemPedido;
+import app.entities.Produto;
 
 import javax.jws.WebService;
 import javax.jws.WebMethod;
@@ -12,8 +13,21 @@ import javax.jws.soap.SOAPBinding.Style;
 @SOAPBinding(style = Style.RPC)
 public interface AppServer {
 	
-	
-    @WebMethod void addItemComanda(Comanda comanda, ItemPedido item);
+	@WebMethod Long putComanda();
+
+    @WebMethod String getComanda(Long idComanda);
+
+    @WebMethod void deleteComanda(Long idComanda);
+
+    @WebMethod Long putProduto(String nmProduto, Double vlProduto);
+
+    @WebMethod Long postProduto(Long idProduto, String newNmProduto, Double newVlProduto);
+
+    @WebMethod String getProduto(Long idProduto);
+
+    @WebMethod void deleteProduto(Long idProduto);
+
+    @WebMethod String addItemComanda(Long idComanda, Long idProduto, Integer qtd);
     
 //    @WebMethod float removeItemComanda(Long idComanda, Long idProduto, Integer qtd);
 //
--- src/main/java/app/AppServerImpl.java
+++ src/main/java/app/AppServerImpl.java
@@ -2,14 +2,79 @@ package app;
 
 import app.entities.Comanda;
 import app.entities.ItemPedido;
+import app.entities.Produto;
 
+import javax.jws.WebMethod;
 import javax.jws.WebService;
+import java.util.HashMap;
 
 @WebService(endpointInterface = "app.AppServer")
 public class AppServerImpl {
 
-    public void addItemComanda(Comanda comanda, ItemPedido item) {
-        comanda.addItemPedido(item);
+    HashMap<Long, Comanda> comandas = new HashMap<>();
+    HashMap<Long, Produto> produtos = new HashMap<>();
+
+    public Long putComanda() {
+        Long idComanda = Long.valueOf(comandas.size() + 1);
+        comandas.put(idComanda, new Comanda(idComanda));
+        return idComanda;
+    }
+
+    public String getComanda(Long idComanda) {
+        if (comandas.containsKey(idComanda)) {
+            return comandas.get(idComanda).toString();
+        }
+        return "Comanda não encontrada!";
+    }
+
+    public void deleteComanda(Long idComanda) {
+        if (comandas.containsKey(idComanda)) {
+            comandas.remove(idComanda);
+        }
+    }
+
+    public Long putProduto(String nmProduto, Double vlProduto) {
+        Long idProduto = Long.valueOf(produtos.size() + 1);
+        produtos.put(idProduto, new Produto(idProduto, nmProduto, vlProduto));
+        return  idProduto;
+    }
+
+    public Long postProduto(Long idProduto, String newNmProduto, Double newVlProduto) {
+        if (produtos.containsKey(idProduto)) {
+            Produto produto =  produtos.get(idProduto);
+            produto.setNome(newNmProduto);
+            produto.setValor(newVlProduto);
+            return idProduto;
+        }
+        return -1L;
+    }
+
+    public String getProduto(Long idProduto) {
+        if (produtos.containsKey(idProduto)) {
+            return produtos.get(idProduto).toString();
+        }
+        return "Produto não encontrado!";
+    }
+
+    public void deleteProduto(Long idProduto) {
+        if (produtos.containsKey(idProduto)) {
+            produtos.remove(idProduto);
+        }
+    }
+
+    public String addItemComanda(Long idComanda, Long idProduto, Integer qtd) {
+        if (comandas.containsKey(idComanda)) {
+            Comanda comanda = comandas.get(idComanda);
+            if (produtos.containsKey(idProduto)) {
+                Produto produto = produtos.get(idProduto);
+                comanda.addItemPedido(new ItemPedido(produto, qtd));
+                return "Item adicionado com sucesso!";
+            } else {
+                return "Produto não encontrado!";
+            }
+        } else {
+            return "Comanda não encontrada";
+        }
     }
 
 
